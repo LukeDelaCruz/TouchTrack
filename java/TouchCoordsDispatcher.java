0ec838fb39a8ea369c8eedc4e9c79697f2350024
Luke Patrick DC (Dela Cruz) <32045517+lukepatr@users.noreply.github.com>
2018-04-04 18:51:09 -0600

    Add files via upload

--- java/TouchCoordsDispatcher.java
+++ java/TouchCoordsDispatcher.java
@@ -3,6 +3,7 @@ package com.example.lukepatrick.touchtrack2;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import okhttp3.Response;
 import okhttp3.WebSocket;
@@ -15,24 +16,42 @@ public class TouchCoordsDispatcher extends WebSocketListener {
         this.view = view;
     }
 
+    boolean moved = false;
+    long startTime = 0;
     @Override
     public void onOpen(final WebSocket webSocket, Response response) {
+
         view.setOnTouchListener(new View.OnTouchListener() {
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.i("TOUCH", motionEvent.toString());
-                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    String coordMessage = String.format("%d, %d", (int) motionEvent.getX(), (int) motionEvent.getY());
-                    webSocket.send(coordMessage);
-                    return true;
+               // Log.i("TOUCH", motionEvent.toString());
+                if (view == null) {
+                    return false;
                 }
-                else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    webSocket.send("Clicked!");
-                    return true;
-                }
+                switch (motionEvent.getActionMasked()) {
+                    case MotionEvent.ACTION_MOVE:
+                        String coordMessage = String.format("%d, %d", (int) motionEvent.getRawX(), (int) motionEvent.getRawY());
+                        webSocket.send(coordMessage);
+                        moved = true;
+                        startTime = System.currentTimeMillis();
 
-                return false;
+//                    case MotionEvent.ACTION_DOWN:
+//                        return true;
+
+//                    case MotionEvent.ACTION_UP:
+//                        if (System.currentTimeMillis() - startTime > 700){
+//                            moved = false;
+//                        }
+//                        else {
+//                            webSocket.send("Clicked!");
+//                        }
+//                    case MotionEvent.ACTION_CANCEL:
+
+                }
+                return true;
             }
         });
     }
 }
+
