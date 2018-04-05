ad91e4f5675b8b2f60fca93be6a8c57c64d56728
Luke Patrick DC (Dela Cruz) <32045517+lukepatr@users.noreply.github.com>
2018-04-04 22:46:58 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -76,7 +76,3 @@ if __name__ == "__main__":
     from geventwebsocket.handler import WebSocketHandler
     server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
     server.serve_forever()
-
-    # received_coords = [[[-1, -2], False], [[-1, -1], False]]
-    # received_coords[0][0][0] = -3
-    # print(received_coords[0][0][0])
