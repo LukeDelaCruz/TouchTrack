70b76d7f792b4033df14d1f0f5405cd451829fad
Luke Patrick DC (Dela Cruz) <32045517+lukepatr@users.noreply.github.com>
2018-04-07 10:13:34 -0600

    Add files via upload

---
+++ codegen.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+# generates a random password of length N cosisting of numbers and letters
+def gen_password(N):
+    word = ''
+    for _ in range(N):
+        word+= random.SystemRandom().choice(string.ascii_letters + string.digits)
+    return word
